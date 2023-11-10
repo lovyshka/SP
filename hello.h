34bdcd4943270a1f0fb43416310053482fa556d2
Арсений Воронов <dejabatg@student.21-school.ru>
2023-11-11 02:05:29 +0300

    i dont understand nothing

--- hello.h
+++ hello.h
@@ -1,7 +1,21 @@
 #ifndef UNTITLED1_HELLO_H
 #define UNTITLED1_HELLO_H
+#define BUFF_SIZE 100
 
 
+#include <stdio.h>
+#include <stdlib.h>
 
+struct input_data{
+    char url_address[BUFF_SIZE];
+    char ip_address[BUFF_SIZE];
+};
+
+union bibki{
+    struct input_data sas;
+    char * arr[sizeof(struct input_data)];
+};
+
+void get_data(struct input_data * input);
 
 #endif //UNTITLED1_HELLO_H
--- main.c
+++ main.c
@@ -1,8 +1,14 @@
-#include <stdio.h>
+#include "hello.h"
 
-int main(){
 
+int main() {
+    struct input_data input = {0};
+    get_data(&input);
 
     return 0;
 }
 
+void get_data(struct input_data * input){
+    printf("Input your data in format \"[url of the site] [ip address]\"\n");
+    scanf("%s %s", input -> url_address, input -> ip_address);
+}
